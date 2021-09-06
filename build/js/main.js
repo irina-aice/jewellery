a86dd130d89fc6b4ac2348d8371eb0e8c0bcca95
irina-aice <61356164+irina-aice@users.noreply.github.com>
2021-09-06 12:23:53 +0300

    in progress

--- build/js/main.js
+++ build/js/main.js
@@ -26,6 +26,10 @@
   const button = document.querySelector('.js-filter-show');
   const FILTER_ID = 'filter';
 
+  if (!button) {
+    return false;
+  }
+
   button.addEventListener('click', (evt) => {
     evt.preventDefault();
 
