a696962b6ac14293f90a943344781936c124e024
irina-aice <61356164+irina-aice@users.noreply.github.com>
2021-10-27 14:41:25 +0000

    Deploying to gh-pages from @ irina-aice/jewellery@b230424301f55693afa205ec9e282f525b3403cb 🚀

---
+++ js/main.js
@@ -0,0 +1,196 @@
+'use strict';
+
+(function () {
+  const collapsibleTitles = document.querySelectorAll('.js-collapsible-title');
+  const collapsibleContent = document.querySelectorAll('.js-collapsible-content');
+
+  collapsibleContent.forEach((content) => {
+    const title = content.previousElementSibling;
+
+    if (title.getAttribute('aria-expanded') === 'true') {
+      return;
+    }
+
+    content.hidden = true;
+  });
+
+  collapsibleTitles.forEach((title) => {
+    const content = title.nextElementSibling;
+
+    title.addEventListener('click', () => {
+      const expanded = title.getAttribute('aria-expanded') === 'true' || false;
+
+      title.setAttribute('aria-expanded', !expanded);
+      content.hidden = expanded;
+    });
+  });
+})();
+
+'use strict';
+
+(function () {
+  const button = document.querySelector('.js-filter-show');
+  const FILTER_ID = 'filter';
+
+  if (!button) {
+    return false;
+  }
+
+  button.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    window.MicroModal.show(FILTER_ID, {
+      openClass: 'filter--open',
+      disableScroll: true,
+    });
+  });
+})();
+
+'use strict';
+
+(function () {
+  const form = document.querySelector('.js-form');
+
+  if (!form) {
+    return false;
+  }
+
+  const emailField = form.querySelector('[name="email"]');
+
+  if (!emailField) {
+    return false;
+  }
+
+  if (localStorage.getItem(emailField.name)) {
+    emailField.value = localStorage.getItem(emailField.name);
+  }
+
+  form.addEventListener('submit', () => {
+    localStorage.setItem(emailField.name, emailField.value);
+  });
+})();
+
+'use strict';
+
+(function () {
+  const links = document.querySelectorAll('a[data-micromodal-trigger]');
+
+  if (!links.length) {
+    return false;
+  }
+
+  links.forEach((link) => {
+    link.addEventListener('click', (evt) => {
+      evt.preventDefault();
+
+      const modalId = link.getAttribute('data-micromodal-trigger');
+
+      window.MicroModal.show(modalId, {
+        openClass: 'modal--open',
+        disableScroll: true,
+      });
+    });
+  });
+})();
+
+'use strict';
+
+(function () {
+  const headerOpenedClass = 'header--opened';
+  const headerClosedClass = 'header--closed';
+  const pageNavOpenedClass = 'page--nav-opened';
+
+  const header = document.querySelector('.js-header');
+  const headerButton = document.querySelector('.js-header-menu-btn');
+  const page = document.querySelector('.js-page');
+
+
+  if (!page || !header || !headerButton) {
+    return false;
+  }
+
+  header.classList.add(headerClosedClass);
+
+  const open = function() {
+    header.classList.remove(headerClosedClass);
+    header.classList.add(headerOpenedClass);
+    page.classList.add(pageNavOpenedClass);
+  };
+
+  const close = function() {
+    header.classList.add(headerClosedClass);
+    header.classList.remove(headerOpenedClass);
+    page.classList.remove(pageNavOpenedClass);
+  };
+
+  headerButton.addEventListener('click', () => {
+    if (header.classList.contains(headerClosedClass)) {
+      open();
+    } else {
+      close();
+    }
+  });
+})();
+
+'use strict';
+
+(function () {
+  document.documentElement.classList.remove('no-js');
+  document.documentElement.classList.add('js');
+})();
+
+'use strict';
+
+(function () {
+  const slider = document.querySelector('.js-slider');
+  const pagination = document.querySelector('.js-slider-pagination');
+  const nextEl = document.querySelector('.js-slider-next');
+  const prevEl = document.querySelector('.js-slider-prev');
+
+  if (!slider || !pagination || !nextEl || !prevEl) {
+    return false;
+  }
+
+  new window.Swiper(slider, {
+    slidesPerView: 2,
+    slidesPerGroup: 2,
+    spaceBetween: 30,
+    pagination: {
+      el: pagination,
+      type: 'fraction',
+      renderFraction: function(currentClass, totalClass) {
+        return `<span class="${currentClass}"></span> <span class="slider__divider">of</span> <span class="${totalClass}"></span>`;
+      },
+    },
+    navigation: {
+      nextEl: nextEl,
+      prevEl: prevEl,
+    },
+    breakpoints: {
+      768: {
+        pagination: {
+          type: 'bullets',
+          clickable: true,
+          bulletClass: 'slider__number',
+          bulletActiveClass: 'slider__number--active',
+          renderBullet: function (index, className) {
+            return `<span class="${className}">${index + 1}</span>`;
+          },
+        },
+      },
+      1024: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        pagination: {
+          type: 'bullets',
+          clickable: true,
+          bulletClass: 'slider__number',
+          bulletActiveClass: 'slider__number--active',
+          renderBullet: function (index, className) {
+            return `<span class="${className}">${index + 1}</span>`;
+          },
+        },
+      },
+    },
+  });
+})();
